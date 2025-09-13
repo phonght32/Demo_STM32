4b3162caa84d6d493a8c385ef523ece22db327be
phonght32 <thanhphongho1998@gmail.com>
2025-09-13 20:57:11 +0700

    remove debug folder

--- STM32F4/STM32F405_Kalman_Height_Estimation/Debug/CLinearAlgebra/Matrix/Matrix.d
+++
@@ -1,4 +0,0 @@
-CLinearAlgebra/Matrix/Matrix.o: \
- C:/Data/Firmware/Demo_STM32/Components/CLinearAlgebra/Matrix/Matrix.c \
- C:/Data/Firmware/Demo_STM32/STM32F4/STM32F405_Kalman_Height_Estimation/../../Components/CLinearAlgebra/Matrix/Matrix.h
-C:/Data/Firmware/Demo_STM32/STM32F4/STM32F405_Kalman_Height_Estimation/../../Components/CLinearAlgebra/Matrix/Matrix.h:
